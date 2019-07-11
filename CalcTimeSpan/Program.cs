610019322bf91753c2fc368fdf59c9bd6697d52a
Yosuke Maeda <yosuke_maeda@hotmail.com>
2019-07-11 13:42:41 +0900

    Add project files.

---
+++ CalcTimeSpan/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Windows;
+
+namespace CalcTimeSpan {
+	class Program {
+		static Dictionary<TimeSpan,TimeSpan> dic = new Dictionary<TimeSpan,TimeSpan>();
+		static TimeSpan total = new TimeSpan();
+		static bool skip = false,quit=false;
+		[STAThread]
+		static void Main(string[] args) {
+			Console.Title="Music title editor";
+			Console.WriteLine("You can (q)uit anytime. Load 'output' file by simply hitting Enter key.");
+			for(;;) {
+				Console.Write("Current TimeSpan : ");
+				Console.ForegroundColor=ConsoleColor.White;
+				Console.WriteLine("{0}",total);
+				Console.ResetColor();
+				for(string duration = Prompt();!isQuit(duration);) {
+					if(duration.Equals("load",StringComparison.CurrentCultureIgnoreCase)) {
+
+					}
+					TimeSpan result;
+					if(Regex.IsMatch(duration,"[0-9]+[:][0-9]+")&&!Regex.IsMatch(duration,"[0-9]+[:][0-9]+[:][0-9]+")) {
+						// add 00: if 00:00 so that we get 00:00:00.
+						duration=String.Format("00:{0}",duration);
+					}
+					if(TimeSpan.TryParse(duration,out result)) {
+						dic.Add(total,result);
+						total+=result;
+						break;
+					} else {
+						Console.Beep();
+						duration=Prompt();
+					}
+				}
+				if(skip||quit) {
+					break;
+				}
+			}
+			//Supplement();
+			fillWithData();
+		}
+		private static bool isQuit(string duration) {
+			return (quit=duration.StartsWith("q",StringComparison.CurrentCultureIgnoreCase)||String.IsNullOrEmpty(duration));
+		}
+		private static string Prompt() {
+			Console.Write("Type a duration (mm:ss) and press Enter key : ");
+			string duration = Console.ReadLine();
+			if(isQuit(duration)) {
+				skip=dic.Count==0;
+#if false
+				if(!skip) {
+					if(!dic.ContainsKey(total)) {
+						dic.Add(total,total);
+					}
+				}
+#endif
+			}
+			return duration;
+		}
+		static void Supplement() {
+			Console.ForegroundColor=ConsoleColor.Yellow;
+			Console.Write("Will you want to have the result in clipboard? (Y/n) : ");
+			Console.ResetColor();
+			ConsoleKeyInfo ans = Console.ReadKey();
+			Console.WriteLine();
+			if(ans.KeyChar!='n') {
+				try {
+					using(StringWriter sw = new StringWriter()) {
+						foreach(KeyValuePair<TimeSpan,TimeSpan> elem in dic) {
+							sw.WriteLine("({0:HH:mm:ss}) : {1:HH:mm:ss}",elem.Key,elem.Value);
+						}
+						Clipboard.SetText(sw.ToString());
+					}
+					Console.ForegroundColor=ConsoleColor.Cyan;
+					Console.WriteLine("Successfully written to the Clipboard.");
+					Console.ResetColor();
+				} catch(Exception ex) {
+					Console.ForegroundColor=ConsoleColor.Red;
+					Console.WriteLine(ex.Message);
+					Console.ResetColor();
+				}
+			}
+		}
+		private static void fillWithData() {
+			Application app = new Application();
+			using(YouTubeMusicHelper win = new YouTubeMusicHelper(dic)) {
+				win.Title=Console.Title;
+				app.Run(win);
+			}
+		}
+	}
+}
---
+++ CalcTimeSpan/YouTubeMusicHelper.cs
@@ -0,0 +1,255 @@
+﻿// $Header: $
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CalcTimeSpan {
+	public class YouTubeMusicHelper:Window,IDisposable {
+		protected Dictionary<TimeSpan,TimeSpan> dics;
+		public FileInfo output = new FileInfo("output.txt");
+		protected bool loaded = false,changed=false;
+		public YouTubeMusicHelper(Dictionary<TimeSpan,TimeSpan> dict) {
+			dics=dict;
+			Width=1024/2;
+			Height=768/2;
+			this.Content=Edit();
+		}
+		object Edit() {
+			ListBox lb = new ListBox();
+			lb.SelectionChanged+=(sender,e) => {
+				ListBoxItem item = e.AddedItems[0] as ListBoxItem;
+				Grid grid = item.Content as Grid;
+				TextBox tb = grid.Children[1] as TextBox;
+				tb.SelectAll();
+				tb.Focus();
+			};
+			lb.HorizontalContentAlignment=HorizontalAlignment.Stretch;
+			if(dics.Count==0&&output.Exists) {
+				using(StreamReader sr = output.OpenText()) {
+					for(int i=1;!sr.EndOfStream;++i) {
+						string line = sr.ReadLine();
+						Match Ma = Regex.Match(line,"^(?<inx>[0-9 ]+)[ ](?<nam>.+)[(](?<one>[0-9]{2}:[0-9]{2})[)][ ](?<two>[0-9]{2}:[0-9]{2}:[0-9]{2})$");
+						if(Ma.Success) {
+							string[] cols = new string[4];
+							cols[0]=i.ToString();// Ma.Groups["inx"].Value.Trim();
+							cols[1]=Ma.Groups["nam"].Value.Trim();
+							cols[2]=Ma.Groups["one"].Value.Trim();
+							cols[3]=Ma.Groups["two"].Value.Trim();
+							KeyValuePair<TimeSpan,TimeSpan> pair;
+							lb.Items.Add(Edit(cols,out pair));
+							dics.Add(pair.Key,pair.Value);
+							loaded=true;
+						}
+					}
+				}
+				Recalculate();
+			} else {
+				int i = 1;
+				foreach(KeyValuePair<TimeSpan,TimeSpan> dic in dics) {
+					lb.Items.Add(Edit(i,dic));
+					++i;
+				}
+				lb.SelectedIndex=0;
+			}
+			return lb;
+		}
+		private object Edit(string[] cols,out KeyValuePair<TimeSpan,TimeSpan> pair) {
+			ListBoxItem item = new ListBoxItem();
+			item.HorizontalContentAlignment=HorizontalAlignment.Stretch;
+			Grid grid = new Grid();
+			grid.HorizontalAlignment=HorizontalAlignment.Stretch;
+			grid.Margin=new Thickness(0,1,0,1);
+			for(int i = 0;i<cols.Length;++i) {
+				grid.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+			for(int i = 0;i<grid.ColumnDefinitions.Count;++i) {
+				grid.ColumnDefinitions[i].Width=GridLength.Auto;
+			}
+			{
+				// Index
+				TextBlock tb = new TextBlock();
+				tb.Text=String.Format("{0,3}",cols[0]);
+				tb.VerticalAlignment=VerticalAlignment.Center;
+				tb.Margin=new Thickness(0,0,2,0);
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,0);
+			}
+			{
+				// Title
+				TextBox tb = new TextBox();
+				tb.Text=cols[1];
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,1);
+				grid.ColumnDefinitions[1].Width=new GridLength(250,GridUnitType.Star);
+				tb.TextChanged+=(sender,e) => {
+					changed=true;
+					loaded=false;
+				};
+			}
+			TimeSpan len=TimeSpan.MinValue, top=TimeSpan.MinValue;
+			{
+				// Total Time
+				TextBlock tb = new TextBlock();
+				tb.Text=String.Format("{0}",cols[2]);
+				tb.HorizontalAlignment=HorizontalAlignment.Right;
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,2);
+				grid.ColumnDefinitions[2].Width=new GridLength(80,GridUnitType.Star);
+				top=TimeSpan.Parse(String.Format("00:{0}",cols[2]));
+			}
+			{
+				// Song Length
+				TextBox tb = new TextBox();
+				tb.Tag=Int32.Parse(cols[0]);
+				tb.Text=String.Format("{0}",cols[3]);
+				tb.HorizontalAlignment=HorizontalAlignment.Right;
+				tb.VerticalAlignment=VerticalAlignment.Center;
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,3);
+				grid.ColumnDefinitions[3].Width=new GridLength(60,GridUnitType.Star);
+				len=TimeSpan.Parse(cols[3]);
+				tb.TextChanged+=(sender,e) => {
+					changed=true;
+					loaded=false;
+					Recalculate(sender,e);
+				};
+			}
+			item.Content=grid;
+			pair=new KeyValuePair<TimeSpan,TimeSpan>(top,len);
+			return item;
+		}
+
+		private void Recalculate(object sender,TextChangedEventArgs e) {
+			Recalculate();
+		}
+		private void Recalculate() {
+			try {
+				dics.Clear();
+				TimeSpan total = new TimeSpan();
+				ListBox lb = this.Content as ListBox;
+				for(int i = 0;i<lb.Items.Count;++i) {
+					ListBoxItem item = lb.Items[i] as ListBoxItem;
+					Grid grid = item.Content as Grid;
+					TimeSpan len = TimeSpan.Parse((grid.Children[3] as TextBox).Text);
+					(grid.Children[2] as TextBlock).Text=String.Format("{0}",total);
+					dics.Add(total,len);
+					total+=len;
+				}
+			}catch(Exception/*ex*/) {
+				//MessageBox.Show(ex.ToString(),this.Title,MessageBoxButton.OK,MessageBoxImage.Warning);
+			}
+		}
+
+		private object Edit(int index,KeyValuePair<TimeSpan,TimeSpan> dic) {
+			ListBoxItem item = new ListBoxItem();
+			item.HorizontalContentAlignment=HorizontalAlignment.Stretch;
+			Grid grid = new Grid();
+			grid.HorizontalAlignment=HorizontalAlignment.Stretch;
+			grid.Margin=new Thickness(0,1,0,1);
+			for(int i = 0;i<4;++i) {
+				grid.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+			for(int i = 0;i<grid.ColumnDefinitions.Count;++i) {
+				grid.ColumnDefinitions[i].Width=GridLength.Auto;
+			}
+			{
+				TextBlock tb = new TextBlock();
+				tb.Text=String.Format("{0,3}",index);
+				tb.VerticalAlignment=VerticalAlignment.Center;
+				tb.Margin=new Thickness(0,0,2,0);
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,0);
+			}
+			{
+				TextBox tb = new TextBox();
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,1);
+				grid.ColumnDefinitions[1].Width=new GridLength(250,GridUnitType.Star);
+			}
+			{
+				TextBox tb = new TextBox();
+				tb.Text=String.Format("{0}",dic.Key);
+				tb.HorizontalContentAlignment=HorizontalAlignment.Right;
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,2);
+				grid.ColumnDefinitions[2].Width=new GridLength(80,GridUnitType.Star);
+			}
+			{
+				TextBlock tb = new TextBlock();
+				tb.Text=String.Format("{0}",dic.Value);
+				tb.HorizontalAlignment=HorizontalAlignment.Right;
+				tb.VerticalAlignment=VerticalAlignment.Center;
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,3);
+				grid.ColumnDefinitions[3].Width=new GridLength(60,GridUnitType.Star);
+			}
+			item.Content=grid;
+			return item;
+		}
+
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing) {
+			if(!disposedValue) {
+				if(disposing) {
+					Save();
+				}
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+				disposedValue=true;
+			}
+		}
+		private void Save() {
+			if(loaded) {
+				return;
+			}
+			if(changed) {
+				if(MessageBoxResult.No==MessageBox.Show("Will you save?",this.Title,MessageBoxButton.YesNo,MessageBoxImage.Question)) {
+					return;
+				}
+			}
+			ListBox Lb = this.Content as ListBox;
+			if(Lb.Items.Count>0) {
+				if(output.Exists) {
+					output.Delete();
+				}
+				using(FileStream fs = output.OpenWrite()) {
+					using(StreamWriter sw = new StreamWriter(fs)) {
+						sw.AutoFlush=true;
+						foreach(ListBoxItem item in Lb.Items) {
+							Grid grid = item.Content as Grid;
+							TextBlock index = grid.Children[0] as TextBlock;
+							sw.Write("{0} ",index.Text);
+							TextBox title = grid.Children[1] as TextBox;
+							sw.Write("{0} ",title.Text);
+							TextBox start = grid.Children[2] as TextBox;
+							TimeSpan time = TimeSpan.Parse(start.Text);
+							if(time.Hours>0) {
+								sw.Write("({0})",time);
+							} else {
+								sw.Write("({0:00}:{1:00})",time.Minutes,time.Seconds);
+							}
+							TextBlock length = grid.Children[3] as TextBlock;
+							sw.Write(" {0}",TimeSpan.Parse(length.Text));
+							sw.WriteLine();
+						}
+						//sw.Flush();
+					}
+				}
+			}
+		}
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~YouTubeMusicHelper() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose() {
+			Dispose(true);
+			//GC.SuppressFinalize(this);
+		}
+	}
+}
